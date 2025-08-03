29b467544dc7cccde8dcdfcb9f6f1627602e0c1b
eze-root <labezeswu@outlook.com>
2025-08-03 20:24:55 +0800

    feat: add main.py

---
+++ bash_scripts/download_node_exporter.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+# https://github.com/prometheus/node_exporter/releases
+# linux-amd64, linux-arm64, darwin-amd64, darwin-arm64
+
+ARCH=${1:-linux-amd64} 
+OUTDIR="exporter/node_exporter"
+TARGET="${ARCH}"
+echo "Downloading Node Exporter for target: $TARGET"
+
+if [ ! -d "$OUTDIR" ]; then
+  mkdir -p "$OUTDIR"
+fi
+
+
+if [ ! -d "$OUTDIR/$ARCH" ]; then
+  mkdir -p "$OUTDIR/$ARCH"
+fi
+
+
+URL=$(curl -s https://api.github.com/repos/prometheus/node_exporter/releases/latest \
+  | grep browser_download_url \
+  | grep "$TARGET" \
+  | grep '.tar.gz' \
+  | cut -d '"' -f 4)
+FILENAME=$(basename "$URL")
+
+curl -L -o "$OUTDIR/$FILENAME" "$URL"
+tar -xzf "$OUTDIR/$FILENAME" -C "$OUTDIR/$ARCH" --strip-components=1
+rm "$OUTDIR/$FILENAME"
+chmod +x "$OUTDIR/$ARCH/node_exporter"
